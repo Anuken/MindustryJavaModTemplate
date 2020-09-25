310bb64d92b597f5d5acbf974b8b17e20fe74055
Anuken <arnukren@gmail.com>
2020-09-25 14:20:14 -0400

    Assets & sprites example

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -7,15 +7,24 @@ import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
 
 public class ExampleJavaMod extends Mod{
 
-    //register event handlers and create variables in the constructor
     public ExampleJavaMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
+        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            Time.runTask(10f, () -> Vars.ui.showInfo("it works. incredib le."));
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
         });
     }
 
