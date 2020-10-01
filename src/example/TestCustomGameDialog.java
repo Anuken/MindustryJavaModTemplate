80307e052809508949ac7433dde5c8de3638df43
Minxyzgo <71958008+Minxyzgo@users.noreply.github.com>
2020-10-01 11:26:10 +0800

    Update TestCustomGameDialog.java

--- src/example/TestCustomGameDialog.java
+++ src/example/TestCustomGameDialog.java
@@ -3,11 +3,9 @@ package example;
 import java.lang.reflect.*;
 import arc.func.*;
 import arc.scene.ui.layout.*;
-import arc.*;
-import arc.util.*;
+
 import mindustry.ui.dialogs.*;
 import static mindustry.Vars.*;
-import static mindustry.game.EventType.*;
 
 public class TestCustomGameDialog {
 	public static void init() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
@@ -16,11 +14,7 @@ public class TestCustomGameDialog {
         Field nameFieldIB = CustomRulesDialog.class.getDeclaredField("main");
         nameFieldIB.setAccessible(true);
         Table ta = (Table)nameFieldIB.get(nameFieldInA.get(ui.custom));
-        Events.on(ClientLoadEvent.class, e -> {
-            Time.runTask(30f, () -> {
-            	check(ta, "ss", b -> {}, () -> {return true;}, () -> {return true;});
-            });
-        });
+	check(ta, "ss", b -> {}, () -> {return true;}, () -> {return true;});
 	}
 	
 	static void check(Table main, String text, Boolc cons, Boolp prov, Boolp condition){
