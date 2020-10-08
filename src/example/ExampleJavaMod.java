0d365919d29eb234b35f3ec20ea2c139ce8ad8ca
AmteurPotion <1k0j2i4@gmail.com>
2020-10-08 21:58:08 +0900

    unnecessary import clean up

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -2,10 +2,7 @@ package example;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
