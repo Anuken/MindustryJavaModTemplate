ed0c9ea41d97079b4505587236a660b96c90e083
AmteurPotion <1k0j2i4@gmail.com>
2020-10-10 02:15:36 +0900

    Edit silver wall

--- src/example/content/ExBlocks.java
+++ src/example/content/ExBlocks.java
@@ -1,8 +1,11 @@
 package example.content;
 
 import mindustry.ctype.ContentList;
+import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.defense.Wall;
+
+import static mindustry.type.ItemStack.*;
 
 public class ExBlocks implements ContentList{
 
@@ -11,7 +14,8 @@ public class ExBlocks implements ContentList{
 
     @Override
     public void load(){
-        silverWall = new Wall("name"){{
+        silverWall = new Wall("silver-wall"){{
+            requirements(Category.defense, with(ExItems.silver, 40));
             size = 2;
             health = 600;
         }};
